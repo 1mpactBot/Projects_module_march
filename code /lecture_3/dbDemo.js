e72bc5c5bc15144c088e765b6b6c35fabc195818
Jasbir <mr.jasbirsingh96@gmail.com>
2024-03-09 22:46:57 +0530

    explained upto mongoose models

--- code /lecture_3/dbDemo.js
+++ code /lecture_3/dbDemo.js
@@ -1,5 +1,68 @@
+const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 dotenv.config();
-const {DB_USER,DB_PASSWORD}=process.env;
-console.log(DB_USER,DB_PASSWORD);
-// const dbURL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.drcvhxp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// it adds all the enviornment variables to processe.env
+const { DB_USER, DB_PASSWORD, LOCAL_PORT } = process.env;
+// create a server
+const app = express();
+/****pre-defined/third party middleware**/
+app.use(express.json());
+const dbURL =
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.drcvhxp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+mongoose.connect(dbURL)
+    .then(function (connection) {
+        console.log(connection);
+    }).catch(err => { console.log(err) });
+
+
+/***
+ * ProductModel -> 
+ * schema
+ *      uniqueID -> mongodb
+ *      item name
+ *      description 
+ *      price
+ *      discount
+ *      
+ * 
+ * 
+ * ***/
+/**
+ * rules that an entity should follow
+ * **/
+const productSchemaRules = {
+    producedBy: String,
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 1
+    },
+    discount: {
+        type: Number,
+        validate: function () {
+            return this.price > this.discount;
+        }
+    }
+}
+const productSchema = new mongoose.Schema(productSchemaRules);
+
+const ProductModel=new mongoose.model("ProductModel",productSchema);
+
+
+
+
+
+const PORT = process.env.PORT || LOCAL_PORT;
+app.listen(PORT, function () {
+    console.log("server is running at port 3000");
+})
+
